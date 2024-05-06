999d3bd52c6e09f89787a3e9592e38c2c050ace0
M S I <M S I@MSI>
2024-05-06 11:14:11 +0700

    int main

--- FriendFunction1/FriendFunction1.cpp
+++ FriendFunction1/FriendFunction1.cpp
@@ -11,3 +11,14 @@ public:
 	friend void setNama(mahasiswa& a, string);
 };
 
+void setNama(mahasiswa& a, string pNama)
+{
+	a.nama = pNama;
+}
+
+int main()
+{
+	mahasiswa joko;
+	setNama(joko, "Kairi Kumar");
+	return 0;
+}
