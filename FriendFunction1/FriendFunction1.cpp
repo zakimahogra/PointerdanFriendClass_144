deab5bfcd983b5d986a07ba1727032d3318bbe56
M S I <M S I@MSI>
2024-05-06 11:13:40 +0700

    membuat class mahasiswa menggunakan friend

--- FriendFunction1/FriendFunction1.cpp
+++ FriendFunction1/FriendFunction1.cpp
@@ -11,12 +11,3 @@ public:
 	friend void setNama(mahasiswa& a, string);
 };
 
-void setNama(mahasiswa& a, string pNama);
-{
-	a.nama = pNama;
-}
-
-int main()
-{
-
-}
---
+++ FriendFunction2/FriendFunction2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class pelajar;
+class manusia
+{
+public:
+	void showNilaiPelajar(pelajar& x);
+};
+
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x);
