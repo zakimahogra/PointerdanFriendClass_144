d3d85be96ed13bfa3efb21b7d722873e43b74f45
M S I <M S I@MSI>
2024-05-06 11:07:13 +0700

    membuat class siswa

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -13,3 +13,29 @@ public:
 	friend class siswa;
 };
 
+class siswa
+{
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a)
+{
+	cout << id << endl
+		<< a.nama;
+}
+
+void orang::setNama(string pNama)
+{
+	nama = pNama;
+}
+
+void siswa::setId(int pId)
+{
+	id = pId;
+}
+
+
