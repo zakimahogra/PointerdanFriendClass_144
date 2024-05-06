e4370ad41223d893fc5f9927b9456356880f266d
M S I <M S I@MSI>
2024-05-06 11:06:10 +0700

    membuat class orang

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -13,37 +13,3 @@ public:
 	friend class siswa;
 };
 
-class siswa
-{
-private:
-	int id;
-public:
-	void setId(int pId);
-	void displayAll(orang& a);
-};
-
-void siswa::displayAll(orang& a)
-{
-	cout << id << endl
-		 << a.nama;
-}
-
-void orang::setNama(string pNama)
-{
-	nama = pNama;
-}
-
-void siswa::setId(int pId)
-{
-	id = pId;
-}
-
-int main()
-{
-	orang joko;
-	joko.setNama("Joko Susilo");
-	siswa joko_siswa;
-	joko_siswa.setId(1);
-	joko_siswa.displayAll(joko);
-	return 0;
-}
